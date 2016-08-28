35ead4be0af462ad711bef26fd567c6b4ff2282e
Suntree <Suntree@users.noreply.github.com>
2016-08-28 19:44:32 +1000

    Update deleteOldImages.sh

--- bin/deleteOldImages.sh
+++ bin/deleteOldImages.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Use the following command to add the next following line to cron
+# sudo crontab -e
+# 10 * * * * /var/www/html/bin/deleteOldImages.sh > /dev/null 2>&1
+
 sudo find /var/www/html/satelliteImages/ -mmin +1500 -exec rm -f {} \;
