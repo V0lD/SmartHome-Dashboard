e0ecf213c680a17e767c746d781a6066874fc82f
V0lD <V0lD@users.noreply.github.com>
2016-08-28 23:35:10 +1000

    Update downloadImages.sh

--- bin/downloadImages.sh
+++ bin/downloadImages.sh
@@ -5,3 +5,5 @@
 # 10 * * * * /var/www/html/bin/downloadImages.sh > /dev/null 2>&1
 
 sudo wget -q -nc --retry-connrefused -P /var/www/html/satelliteImages/ $(eval echo http://rammb.cira.colostate.edu/ramsdis/online/images/himawari-8/australia_true_color/australia_true_color_{$(date --date="-2 hour" +'%Y')..$(date +'%Y')}{$(date --date="-2 hour" +'%m')..$(date +'%m')}{$(date --date="-2 hour" +'%d')..$(date +'%d')}{$(date --date="-2 hour" +'%H')..$(date +'%H')}{0..5}000.jpg)
+
+ 
