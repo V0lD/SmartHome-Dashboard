ce66aa31fb56561cfa6cee954e7d8fd9c374be31
V0lD <V0lD@users.noreply.github.com>
2016-09-05 19:50:49 +1000

    Update deleteOldImages.sh

--- bin/deleteOldImages.sh
+++ bin/deleteOldImages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Use the following command to add the next following line to cron
+# Use the following command to add the next following line to cron to run every 10 minutes
 # sudo crontab -e
-# 10 * * * * /var/www/html/bin/deleteOldImages.sh > /dev/null 2>&1
+# */10 * * * * /var/www/html/bin/deleteOldImages.sh > /dev/null 2>&1
 
 sudo find /var/www/html/satelliteImages/ -mmin +1500 -exec rm -f {} \;
