89f2189a4f215a53f5ffce678a93f9508a772e1b
V0lD <V0lD@users.noreply.github.com>
2016-09-05 19:51:34 +1000

    Update downloadImages.sh

--- bin/downloadImages.sh
+++ bin/downloadImages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Use the following command to add the next following line to cron
+# Use the following command to add the next following line to cron to run every 10 minutes
 # sudo crontab -e
-# 10 * * * * /var/www/html/bin/downloadImages.sh > /dev/null 2>&1
+# */10 * * * * /var/www/html/bin/downloadImages.sh > /dev/null 2>&1
 
 sudo wget -q -nc --retry-connrefused -P /var/www/html/satelliteImages/ $(eval echo http://rammb.cira.colostate.edu/ramsdis/online/images/himawari-8/australia_true_color/australia_true_color_{$(date --date="-2 hour" +'%Y')..$(date +'%Y')}{$(date --date="-2 hour" +'%m')..$(date +'%m')}{$(date --date="-2 hour" +'%d')..$(date +'%d')}{$(date --date="-2 hour" +'%H')..$(date +'%H')}{0..5}000.jpg)
 
