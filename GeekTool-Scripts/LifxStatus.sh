13c002fbacbd8e1a64edfd688cad07ec14e95b33
V0lD <V0lD@users.noreply.github.com>
2016-10-15 12:38:37 +1100

    Update LifxStatus.sh

--- GeekTool-Scripts/LifxStatus.sh
+++ GeekTool-Scripts/LifxStatus.sh
@@ -3,14 +3,27 @@
 # Change your "Success Image" and "Failure Image" on your GeekTool to suit your needs
 
 # Change the IP for your bulb
-IP="192.168.6.6"
+IP="192.168.6.249"
+
+if ! ping -c 1 -t 1 $IP &> /dev/null
+then
+	exit 1
+fi
 
 GetPower="\x24\x00\x00\x34\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x74\x00\x00\x00"
 
-reply=$(echo -e "$GetPower" | nc -w 1 -u $IP 56700)
-power=$(echo -n $reply | tail -c 2 | xxd -p )
+power=$(echo -e "$GetPower" | nc -w 1 -u $IP 56700 | tail -c 2 | xxd -p)
 
-if [ $power = "ffff" ]; then
+#echo $power
+while [ -z "$power" ]
+do
+	#echo ${#power}
+	power=$(echo -e "$GetPower" | nc -w 1 -u $IP 56700 | tail -c 2 | xxd -p);
+done
+
+#echo $power
+
+if [ $power = "ffff" ] ; then
 	exit 0
 else
 	exit 1
